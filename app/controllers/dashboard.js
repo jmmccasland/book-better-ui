bb2cd0b1d3b4474eb113a88dafcec1d99d56945b
John Michael McCasland <jmmccasland@gmail.com>
2019-04-30 22:08:43 -0500

    feat: can create new tour

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -2,10 +2,29 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
   modalOpen: false,
+  title: '',
+
+  reset() {
+    this.set('title', '');
+  },
 
   actions: {
     toggleModal() {
       this.toggleProperty('modalOpen');
+    },
+
+    createTour(ev) {
+      ev.preventDefault();
+
+      let tour = this.store.createRecord('tour', {
+        title: this.title
+      });
+
+      tour.save();
+
+      this.reset();
+      this.set('modalOpen', false);
+
     }
   }
 });
